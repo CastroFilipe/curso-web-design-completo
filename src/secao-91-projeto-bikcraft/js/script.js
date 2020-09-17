cf90e7c9ccdf2eee3da5d5f7cb66047b150e3fae
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-09-17 12:00:10 -0300

    Adição de plug-in js para a criação de slides

---
+++ src/secao-91-projeto-bikcraft/js/script.js
@@ -0,0 +1,15 @@
+//verificação para evitar que páginas que possuem apenas animações e não possuem slide apresentem erro no console.
+if (window.SimpleSlide) {
+    new SimpleSlide({
+        slide: "quote", // nome do atributo data-slide="quote"
+        time: 5000, // tempo de transição dos slides
+    });
+
+    new SimpleSlide({
+        slide: 'galeria', // nome do atributo data-slide="principal"
+        nav: true, // se deve ou não mostrar a navegação
+        auto: true, // se o slide deve passar automaticamente
+        time: 5000, // tempo de transição dos slides
+        pauseOnHover: true, // pausa a transição automática ao passar o mouse
+    });
+}
